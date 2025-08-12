0f062dc214a7de715b8c0c9e85b4aecb85a64e44
odiegosilva1 <odiegosilvadev@gmail.com>
2025-08-12 17:15:59 -0300

    change:jogo_velha.cpp

--- exe03/jogo_velha.cpp
+++ exe03/jogo_velha.cpp
@@ -1,2 +1,18 @@
-// Escopo e váriaveis
-// Exercício 3
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "\tJOGO DA VELHA\n";
+  cout << "\t |  |  | \n";
+  cout << "\t---------\n";
+  cout << "\t |  |  | \n";
+  cout << "\t---------\n";
+  cout << "\t |  |  | \n\n";
+
+  // Pausa (funciona em Linux e Windows)
+  cout << "Pressione Enter para sair...";
+  cin.ignore();
+
+  return 0;
+}
